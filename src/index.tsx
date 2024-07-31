0bc0421f2658fbffc42589ffa94c7c648aa2ddba
Tamara Robichet <v0ld3m0r7@gmail.com>
2024-07-31 10:28:22 +0200

    ✨ (feat): Add UG tab search

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,25 @@
-import { Detail } from "@raycast/api";
+import { Action, ActionPanel, Detail, LaunchProps, List } from "@raycast/api";
 
-export default function Command() {
-  return <Detail markdown="# Hello World" />;
+type Arguments = {
+  query: string;
+};
+
+export default function Command(props: LaunchProps<{ arguments: Arguments }>) {
+  return (
+    <>
+      <List navigationTitle="Search guitar tabs">
+        <List.Item
+          title="🎸 Search Ultimate Guitar"
+          subtitle={props.arguments.query}
+          actions={
+            <ActionPanel title="Tab search">
+              <Action.OpenInBrowser
+                url={`https://www.ultimate-guitar.com/search.php?search_type=title&value=${props.arguments.query}`}
+              />
+            </ActionPanel>
+          }
+        />
+      </List>
+    </>
+  );
 }
